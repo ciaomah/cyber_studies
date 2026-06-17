a4aa046447c5944752dd3cefbb2a43cf6d4904bd
hellommay <hellommay@hotmail.com>
2026-06-17 16:51:31 -0300

    desafio media de notas

---
+++ f02/ferramentas.py
@@ -0,0 +1,17 @@
+def media(notas):
+    soma = 0
+    for nota in notas:
+        soma = soma + nota
+
+    resultado_media = soma / len(notas)
+    return resultado_media
+
+def classificar(nota):
+    if nota >= 90:
+        return "louvor"
+    elif nota >= 70:
+        return "aprovado"
+    else:
+        return "reprovado"
+
+print(classificar(media([85, 92, 78])))
