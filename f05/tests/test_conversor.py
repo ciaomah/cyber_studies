dc4ee4ec5a9240637c8074eec4c6daed75f0a149
hellommay <hellommay@hotmail.com>
2026-07-06 17:35:23 -0300

    adiciona pasta testes e app - aula

---
+++ f05/tests/test_conversor.py
@@ -0,0 +1,8 @@
+from app.conversor import celsius_para_fahrenheit, fahrenheit_para_celsius
+
+def test_celsius_para_fahrenheit():
+    assert celsius_para_fahrenheit(0) == 32
+    assert celsius_para_fahrenheit(100) == 212
+
+def test_fahrenheit_para_celsius():
+    assert fahrenheit_para_celsius(32) == 0
