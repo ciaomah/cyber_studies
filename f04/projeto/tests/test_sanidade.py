51dd0a3dfd01c7e037ea036fca20fa9700a5a199
hellommay <hellommay@hotmail.com>
2026-07-11 14:33:15 -0300

    test: adiciona teste de sanidade

---
+++ f04/projeto/tests/test_sanidade.py
@@ -0,0 +1,8 @@
+from app.app import mensagem_boas_vindas
+
+
+def test_mensagem_boas_vindas():
+    assert mensagem_boas_vindas() == {
+        "esquadrao": "Bem vindo!",
+        "status": "Online",
+    }
